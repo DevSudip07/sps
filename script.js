67856af1ec66d0562ea27b3e16105abd3ed2c9bb
Sudip <sudipdasghosh64@gmail.com>
2025-01-31 12:26:10 +0530

    responsive done

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ let computerPoint = 0;
 
 function userImageShow() {
     images.forEach((image, index) => {
+        // IMAGE DISPLAY CODE HERE
         image.addEventListener('click', () => {
             userImage.src = `./img/${index + 1}.jpg`;
             let userIndex = index + 1;
             let computerIndex = Math.floor(Math.random() * 3) + 1;
             computerImage.src = `./img/${computerIndex}.jpg`;
 
+            // GAME STATUS CODE HERE
             if(userIndex == computerIndex) {
                 gameStatus.textContent = "It's a tie!";
             }else if (userIndex == 1 && computerIndex == 2 || userIndex == 2 && computerIndex == 3 || userIndex == 3 && computerIndex == 1) {
@@ -29,12 +31,13 @@ function userImageShow() {
                 computerScore.textContent = computerPoint;
             }
 
+            // POP UP CODE HERE
             if(computerPoint >= 10 && userPoint < 10 || computerPoint < 10 && userPoint >= 10) {
                 popUp.style.display = 'block';
             }else {
                 popUp.style.display = 'none';
             }
-
+            // PLAY AGAIN BUTTON CODE HERE
             playAgain.addEventListener('click', ()=> {
                 popUp.style.display = 'none';
                 window.location.reload();
